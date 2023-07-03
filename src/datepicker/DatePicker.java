be1768f42379ac2f70b4990c99cebf0f7b247014
prmc102 <primecommunity102@gmail.com>
2023-07-03 11:41:59 +0100

    Date Picker Code Updates

--- src/datepicker/DatePicker.java
+++ src/datepicker/DatePicker.java
@@ -22,19 +22,18 @@ public class DatePicker extends Utility {
 
     @Test
     public void selectDate() {
-        String year = "2025\n1";
+        String year = "2025";
         String month = "May";
         String date = "25";
 
         // Open the celender
         clickOnElement(By.id("onwardCal"));
         while (true) {
-            String monthYear = driver.findElement(By.cssSelector("body > section:nth-child(3) > div:nth-child(3) > main:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)")).getText();
-            System.out.println(monthYear);
+            String monthYear = driver.findElement(By.xpath("//div[@class='DayNavigator__CalendarHeader-qj8jdz-1 fxvMrr']//div[2]")).getText();
             String[] a = monthYear.split(" ");
             String mon = a[0];
-            String yer = a[1];
-            if (mon.equalsIgnoreCase(month) && yer.contains(year)) {
+            String yer = a[1].split("\n")[0];
+            if (mon.equalsIgnoreCase(month) && yer.equalsIgnoreCase(year)) {
                 break;
             } else {
                 clickOnElement(By.xpath("//div[@class='DayNavigator__CalendarHeader-qj8jdz-1 fxvMrr']//div[3]//*[name()='svg']"));
@@ -42,7 +41,7 @@ public class DatePicker extends Utility {
         }
 
         // Select the Date
-        List<WebElement> allDates = driver.findElements(By.xpath("//div[@id='rb-calendar_onward_cal']//table//td"));
+        List<WebElement> allDates = driver.findElements(By.xpath("//div[@class='DatePicker__MainBlock-sc-1kf43k8-1 hHKFiR']/div[3]/div/span/div/span"));
         for (WebElement dt : allDates) {
             if (dt.getText().equalsIgnoreCase(date)) {
                 dt.click();
