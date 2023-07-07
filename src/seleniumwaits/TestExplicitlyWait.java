7687bbe471daaa1addf03b5c41bab84a8e303f1b
prmc102 <primecommunity102@gmail.com>
2023-07-07 14:08:47 +0100

    Added ActionsClass, Javascript, MultipleWindow,Waits Class updates

---
+++ src/seleniumwaits/TestExplicitlyWait.java
@@ -0,0 +1,30 @@
+package seleniumwaits;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import utilities.Utility;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class TestExplicitlyWait extends Utility {
+    String baseUrl = "https://www.letskodeit.com/practice";
+
+    @Before
+    public void setUp() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void explicitlyWaitExample(){
+
+    }
+
+
+    @After
+    public void tearDown() {
+//        closeBrowser();
+    }
+
+}
